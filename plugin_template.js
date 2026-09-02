798e00959ecce4cdc2b1be0e6b463672ade6339e
RioMurata <rmurata@brightcove.com>
2026-09-02 11:22:39 +0100

    Create plugin_template.js

---
+++ plugin_template.js
@@ -0,0 +1,11 @@
+videojs.registerPlugin('myPlugin', function() {
+    var player = this,
+
+        overlay = document.createElement('p');
+    overlay.className = 'vjs-overlay';
+    overlay.innerHTML = "Becoming a plugin developer";
+    player.el().appendChild(overlay);
+});
+
+
+videojs.getPlayer('myPlayerID').myPlugin();
